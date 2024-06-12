b969e3960873ded4067e91373ced84f1205ecbca
Rafael Marques Chaves <rafaelmchaves@gmail.com>
2024-06-12 15:37:18 -0300

    Insert data of operation type into H2 database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into operation_types (id, description, multiplier) values (1, 'COMPRA A VISTA', -1)
+insert into operation_types (id, description, multiplier) values (2, 'COMPRA PARCELADA', -1)
+insert into operation_types (id, description, multiplier) values (3, 'SAQUE', -1)
+insert into operation_types (id, description, multiplier) values (4, 'PAGAMENTO',  1)
